e1ec0760f3118467705b18fd9f147bc3160578eb
Fabien Pinel <fabienpinel@Fabiens-MacBook-Pro.local>
2016-01-07 10:12:18 +0100

    Frontend ajout de login factory et dun peu de code dans le appJS

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('TopMix', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
@@ -10,4 +10,37 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
       $.material.init();
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+app.run([ '$rootScope', 'LoginFactory','$location', function ($rootScope, LoginFactory, $location){
+  $rootScope.logged = false;
+  $rootScope.user = null;
+
+  /************ Global Logout ************/
+  $rootScope.logout = function () {
+    LoginFactory.logout();
+    $rootScope.logged = false;
+    $rootScope.user = null;
+    $location.path('/');
+    window.location.reload();
+  };
+  /************ Global Login ************/
+  $rootScope.login = function(mail, pass){
+    LoginFactory.login(mai, pass).then(
+        function(data){
+          //success
+          $rootScope.logged = true;
+          $rootScope.user = data;
+        },
+        function(message){
+          //error
+          console.log("Error Login: "+message);
+        }
+    )
+  };
+
+  if (window.localStorage.getItem('topmix_usermail') && window.localStorage.getItem('topmix_userpassword')) {
+    $rootScope.login(window.localStorage.getItem('topmix_usermail') , window.localStorage.getItem('topmix_userpassword') );
+  }
 }]);
--- frontend/app/services/login.js
+++ frontend/app/services/login.js
@@ -1,3 +1,43 @@
 /**
  * Created by fabienpinel on 07/01/16.
  */
+app.factory('LoginFactory', ['$http', '$q', function ($http, $q) {
+    var factory = {
+        logged: false,
+        data: false,
+        login: function (mail, password) {
+            var object = {
+                mail: mail,
+                password: password
+            };
+            var deferred = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'http://localhost/login',
+                data: object,
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status) {
+                if (data.status == "success") {
+                    factory.logged = true;
+                    factory.data = true;
+                    window.localStorage.setItem('topmix_usermail', mail);
+                    window.localStorage.setItem('topmix_userpassword', password);
+                    deferred.resolve(data);
+                } else {
+                    deferred.reject(data);
+                }
+            }).error(function (data, status) {
+                deferred.reject('Erreur de connexion !');
+            });
+            return deferred.promise;
+        },
+        logout: function () {
+            factory.data = false;
+            factory.logged = false;
+            window.localStorage.removeItem('topmix_usermail');
+            window.localStorage.removeItem('topmix_userpassword');
+
+        }
+    };
+    return factory;
+}]);
