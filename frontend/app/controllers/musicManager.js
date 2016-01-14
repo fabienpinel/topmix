537321c599ca4f078135510e636477386256d88b
Fabien Pinel <fabienpinel@yahoo.fr>
2016-01-14 10:02:16 +0100

    ngaudio + update readme

--- frontend/app/controllers/musicManager.js
+++ frontend/app/controllers/musicManager.js
@@ -4,11 +4,16 @@
 app.controller("MusicManagerController" , ['$scope', '$rootScope',"$location", function($scope,$rootScope, $location) {
     //nx.add("slider", {parent : "multipisteVolume", label: "coucou",height: 200, width: 80});
     nx.onload = function() {
-        /*
+
          sliderPiste1.hslider = false;
          sliderPiste1.val.value = 50;
          sliderPiste1.draw();
-         */
-    }
+
+    };
     nx.colorize("#3399FF");
+
+
+
+
+
 }]);
