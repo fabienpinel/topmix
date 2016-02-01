d763d614de5e1f9b925141671779595ccd2e19b5
Fabien Pinel <fabienpinel@yahoo.fr>
2016-02-01 14:03:20 +0100

    timeouts okay

--- frontend/app/controllers/musicManager.js
+++ frontend/app/controllers/musicManager.js
@@ -5,6 +5,8 @@ app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory
 
     $scope.mix = {};
     $scope.paused = true;
+    $scope.allTheTimeouts = [];
+    $scope.currentTimeout;
     $.material.init();
 
 
@@ -84,12 +86,14 @@ app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory
     $scope.play = function(line, index){
         //play
         if ($scope.paused) {
-            setTimeout(function () {
+            $scope.currentTimeout = setTimeout(function () {
                 if (++index < line.song.length && !$scope.paused) {
                     line.song[index].file.play();
                     $scope.play(line, index);
                 }
             }, (line.song[index].file.getDuration() * 1000));
+            $scope.allTheTimeouts.push($scope.currentTimeout);
+            console.log("pushing a timeout", $scope.allTheTimeouts);
         }
     };
 
@@ -120,6 +124,13 @@ app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory
                     }
                 });
             });
+            //loop on the timeouts in order to erase all of them
+            $scope.allTheTimeouts.forEach(function(to){
+                clearTimeout(to);
+            });
+            $scope.allTheTimeouts = [];
+
+
             $scope.paused = true;
         }
     };
@@ -194,7 +205,7 @@ app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory
     };
 
     $scope.resetSliders = function() {
-        console.log("c'est la fête");
+        console.log("c'est la fï¿½te");
     }
 
     $scope.$watch('mix', function() {
