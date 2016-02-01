accb59a1d5b70f0ec7464be98d3625e0145ebc03
jeremy <roumegue.jeremy@gmail.com>
2016-02-01 17:49:40 +0100

    égalisation des pistes

--- frontend/app/directives/dragPiste.js
+++ frontend/app/directives/dragPiste.js
@@ -9,8 +9,16 @@ app.directive('dragPiste', function(SamplesFactory) {
 
             $scope.$on('singleMix', function (name, data) {
                 $scope.tracks = data.tracks;
+                var max = 0;
+
                 $scope.tracks.forEach(function (track) {
-                    for (var i = 0; i < 10; i++) track.samples.push(null);
+                    max = track.samples.length>max?track.samples.length:max;
+                });
+
+                if(max < 10) max = 10;
+
+                $scope.tracks.forEach(function (track) {
+                    for (var i = 0; i < max - track.samples.length; i++) track.samples.push(null);
                 });
             });
 
