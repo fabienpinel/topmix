bf6afb5958fbb86ae867faed8aa91603406844b8
Maxime Dito <maxime.dito@gmail.com>
2016-02-04 10:53:42 +0100

    Ajout de socket coté client

--- frontend/app/controllers/musicManager.js
+++ frontend/app/controllers/musicManager.js
@@ -1,7 +1,7 @@
 /**
  * Created by fabienpinel on 11/01/16.
  */
-app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory, TracksFactory, SamplesFactory, $stateParams, $timeout) {
+app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory, TracksFactory, SamplesFactory, $stateParams, $timeout, socket) {
 
     $scope.mix = {};
     $scope.paused = true;
@@ -188,6 +188,10 @@ app.controller("MusicManagerController" , function($scope, ngAudio, MixesFactory
 
     $scope.$on('singleMix', function (name, data) {
         $scope.mix = data;
+    });
+
+    socket.on('mixChange', function() {
+        getMix();
     });
 
     getMix();
